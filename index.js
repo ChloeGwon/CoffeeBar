e2025055cc795e207ab9e2fb6930b0d51b538ecb
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-06-19 14:35:42 -0700

    adding nav bar and front img

---
+++ index.js
@@ -0,0 +1,27 @@
+const popUp = document.getElementById('popUp');
+const close = document.querySelector('.close');
+const subscribeBtn = document.querySelector('.subscribeBtn');
+let subscribe = document.getElementsByClassName('subscribe')[0];
+console.log(subscribe);
+
+subscribe.addEventListener("click", function(){
+    
+
+    popUp.style.display="block";
+
+   
+
+subscribeBtn.addEventListener("click", () => {
+    var email = document.querySelector('#email').value;
+    if (email == '') {
+        document.querySelector('#required').style.visibility = "visible";
+        // body.appendChild(warning);
+    }
+})
+
+
+close.addEventListener("click", () => {
+    popUp.style.display = "none";
+})
+})
+
