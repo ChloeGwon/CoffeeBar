f72c6544303c0d71a0428769c9986e47d4ca2f50
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-07-04 20:56:45 -0700

    slider

--- index.js
+++ index.js
@@ -22,5 +22,44 @@ subscribeBtn.addEventListener("click", () => {
 close.addEventListener("click", () => {
     popUp.style.display = "none";
 })
-})
+});
 
+
+
+const items= [
+    {
+        id:1,
+        name:"Air Force",
+        price: 119,
+        
+    },
+
+    {
+        id:2,
+        title:"Jordan",
+        price: 149,
+        
+    },
+
+    {
+        id:3,
+        title:"Blazer",
+        price: 109,
+       
+    }
+   
+
+];
+
+const wrapper = document.querySelector(".specialWrapper");
+let index=1;
+function amination(){
+    
+    wrapper.style.transform=`translateX(${-100*index}vw)`;
+    index++;
+    if(index==3){
+        index=0;
+    }
+}
+
+let start = setInterval(amination, 3000);
